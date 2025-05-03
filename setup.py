c33d042d7a2b0aebfbf7d250559e7b70e7948bd7
John Fontaine <john@classedu.com>
2025-05-03 12:25:13 -0400

    update for setuptools

--- setup.py
+++ setup.py
@@ -37,8 +37,10 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Topic :: Artificial Intelligence",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Software Development",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
